9ea32c04975e5430ba4d3081c867f8aac8d66640
brhmoz <brhmozkr@gmail.com>
2021-06-03 16:13:20 +0300

    Ikinci commit

--- src/gitPaketWD/BirinciGun.java
+++ src/gitPaketWD/BirinciGun.java
@@ -3,7 +3,8 @@ package gitPaketWD;
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		System.out.println("11:05 - Birinci Gun...");
+		System.out.println("13:57 - Birinci Gun...");
+		System.out.println("Ikinci Commit");
 
 	}
 
