abff52194b683c1e53e74498b90406b57cd1d05f
brhmoz <81961491+brhmoz@users.noreply.github.com>
2021-06-03 18:18:15 +0300

    Update BirinciGun.java

--- src/gitPaketWD/BirinciGun.java
+++ src/gitPaketWD/BirinciGun.java
@@ -5,6 +5,8 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("13:57 - Birinci Gun...");
 		System.out.println("Ikinci Commit");
+		System.out.println("10:11 - Ikinci gun...");
+		
 
 	}
 
